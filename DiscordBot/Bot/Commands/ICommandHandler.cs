057070179dbb60220a6090d706f8e34c572ff6ae
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2021-11-18 08:38:07 +0000

    Add new command handler for registering command modules, and add DI for handler

---
+++ DiscordBot/Bot/Commands/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace DiscordBot.Bot.Commands {
+    public interface ICommandHandler {
+        Task InstallCommandsAsync();
+        Task HandleCommandAsync(SocketMessage messageParam);
+    }
+}
